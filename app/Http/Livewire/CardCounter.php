adcf39ac90930d34eb1708c81286496cd143e041
jocelinkisenga <kisengajocelin7@gmail.com>
2023-01-04 11:23:57 +0100

    added counter

---
+++ app/Http/Livewire/CardCounter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CardCounter extends Component
+{
+    public function render()
+    {
+        return view('livewire.card-counter');
+    }
+}
