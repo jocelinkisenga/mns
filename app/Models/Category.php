af319529ae098e80aee5327e631b867f14f4e892
kisenga shuka jocelin <72705409+jocelinkisenga@users.noreply.github.com>
2025-03-17 11:51:45 +0200

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Category extends Model
 {
     use HasFactory, SoftDeletes;
-    protected $fillable = ['name','visible', 'icon'];
+    protected $fillable = ['name','visible'];
 
     public function products(){
         return $this->hasMany(Product::class);
