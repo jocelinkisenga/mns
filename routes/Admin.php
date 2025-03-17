0a6a7821026aef40e32cd6a4f6cca59c52881ab7
kisenga shuka jocelin <72705409+jocelinkisenga@users.noreply.github.com>
2025-03-17 10:36:50 +0200

    Update Admin.php

--- routes/Admin.php
+++ routes/Admin.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::middleware(['auth','admin'])->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('/admindashboard',[HomeController::class,'index'])->name('admin.dashboard');
     Route::get('/commandes',[CommandeController::class,'index'])->name('admin-commandes');
 
